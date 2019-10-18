e0391b68e86a38e715b74bca1a0a78a30b090511
Pablo Medina Suarez <zacro.suarez@gmail.com>
2019-10-18 11:48:45 +0200

    Updated download_model.sh

--- data/download_model.sh
+++ data/download_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-FILE_ID="16g_imw_KfOQwwKTzC7O_MU6SRkkoHSZ9"
-FILENAME="ssd-head-detector.tar.gz"
+FILE_ID="12cqKTPtQBAu780219hEbST7VwQuf6xDH"
+FILENAME="ssd512-hollywood-trainval-bs_16-lr_1e-05-scale_pascal-epoch-187.h5"
 
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${FILE_ID}" -o ${FILENAME} && tar -xzvf ${FILENAME} && rm -f ${FILENAME} ./cookie
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${FILE_ID}" -o ${FILENAME} && rm -f ./cookie
